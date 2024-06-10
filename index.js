b4153fdc44d11dcdf6ac8c5db1f80e0e39e27652
Jason Korol <ejkorol@gmail.com>
2024-06-10 16:30:17 -0400

    imported video route into server

--- index.js
+++ index.js
@@ -2,15 +2,20 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 
+import videoRoutes from "./routes/videos.js";
+
 const PORT_ALT = 5051;
 const PORT = process.env.PORT || PORT_ALT;
 
 const api = express();
 
 api.use(cors());
+api.use(express.json());
+
+api.use("/videos", videoRoutes);
 
 api.get("/", (_req, res) => {
-  res.send("ok");
+  res.send("ok, root route");
 });
 
 api.listen(PORT, () => {
